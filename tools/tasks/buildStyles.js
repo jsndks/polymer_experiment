1bdd4713b34295e0e62183dade0617df4c867bc0
Jason Dicks <jtd361@gmail.com>
2015-01-30 15:38:50 -0600

    resolved merge conflict

--- tools/tasks/buildStyles.js
+++ tools/tasks/buildStyles.js
@@ -29,7 +29,7 @@ module.exports = function(grunt) {
                     expand: true,
                     cwd: '<%= env.DIR_SRC %>',
                     dest: '<%= env.DIR_DEST %>',
-                    src: ['assets/components/**/*.css']
+                    src: ['assets/components/**/**/*.css']
                 }]
             }
         },
